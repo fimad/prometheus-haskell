ddd1ae58a4242a417445b234de9023f2d3db1fa3
Will Coster <willcoster@gmail.com>
2015-04-11 19:07:13 -0700

    Enforce unique metric names
    The register function will now fail when attempting to register a new
    metric with the name of an existing metric.

--- src/Prometheus/Registry.hs
+++ src/Prometheus/Registry.hs
@@ -23,14 +23,29 @@ globalRegistry :: STM.TVar Registry
 globalRegistry = unsafePerformIO $ STM.newTVarIO []
 
 register :: MetricDesc s -> IO (Metric s)
-register desc = do
+register desc = STM.atomically $ do
     -- Force the metric descriptor to be evaluated. This allows errors returned
     -- by descriptor creating functions to be thrown on registration instead of
     -- at the first use of the metric.
-    stateTVar <- desc `seq` STM.newTVarIO (descInitial desc)
+    stateTVar <- desc `seq` STM.newTVar (descInitial desc)
+    registry <- STM.readTVar globalRegistry
     let addToRegistry = (MkRegisteredMetric (desc, stateTVar) :)
-    STM.atomically $ STM.modifyTVar' globalRegistry addToRegistry
+    checkName registry desc $ STM.modifyTVar' globalRegistry addToRegistry
     return $ Metric $ STM.atomically . STM.modifyTVar' stateTVar
+
+checkName :: Registry -> MetricDesc s -> a -> a
+checkName registry newDesc r = foldl check r $ registry
+    where
+        check a (MkRegisteredMetric (desc, _))
+            | newName == nameOf desc = error errorMessage
+            | otherwise              = a
+        nameOf = metricName . descInfo
+        newName = nameOf newDesc
+        errorMessage = concat [
+                "A metric with the name '"
+            ,   newName
+            ,   "' has already been registered."
+            ]
 
 unsafeRegister :: MetricDesc s -> Metric s
 unsafeRegister = unsafePerformIO . register
@@ -44,7 +59,8 @@ dumpMetrics = do
 dumpMetric :: RegisteredMetric -> IO LBS.ByteString
 dumpMetric (MkRegisteredMetric (desc, stateTVar)) = do
     state <- STM.atomically $ STM.readTVar stateTVar
-    return $ prefix `LBS.append` descDump desc [] info state
+    let dump = descDump desc [] info state
+    return $ if LBS.null dump then LBS.empty else prefix `LBS.append` dump
     where
         ty = descType desc
         info = descInfo desc
