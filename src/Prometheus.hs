4cc3ae6dd9c7be948891a97690479dd25c4cf2cb
Will Coster <willcoster@gmail.com>
2015-04-11 19:12:45 -0700

    Add LabelN type synonyms
    The LabelN types are synonyms for tuples of Strings with a cardinality
    of N. These types allow for the types of vector metrics to be more
    easily written.
    
    For example,
    
    myMetric :: Metric (Vector Label4 Counter)
    
    compared to,
    
    myMetric :: Metric (Vector (String, String, String, String) Counter)

--- src/Prometheus.hs
+++ src/Prometheus.hs
@@ -1,9 +1,8 @@
 module Prometheus (
     Info (..)
-,   Label (..)
-,   LabelPairs
 ,   Metric
 ,   MetricDesc
+
 ,   MetricT
 ,   MonadMetric
 
@@ -27,6 +26,19 @@ module Prometheus (
 ,   Vector
 ,   vector
 ,   withLabel
+
+,   Label (..)
+,   LabelPairs
+,   Label0
+,   Label1
+,   Label2
+,   Label3
+,   Label4
+,   Label5
+,   Label6
+,   Label7
+,   Label8
+,   Label9
 ) where
 
 import Prometheus.Info
--- src/Prometheus/Label.hs
+++ src/Prometheus/Label.hs
@@ -1,6 +1,16 @@
 module Prometheus.Label (
     Label (..)
 ,   LabelPairs
+,   Label0
+,   Label1
+,   Label2
+,   Label3
+,   Label4
+,   Label5
+,   Label6
+,   Label7
+,   Label8
+,   Label9
 ) where
 
 
@@ -9,34 +19,52 @@ type LabelPairs = [(String, String)]
 class Ord l => Label l where
     labelPairs :: l -> l -> LabelPairs
 
+type Label0 = ()
+
 instance Label () where
     labelPairs () () = []
+
+type Label1 = String
 
 instance Label String where
     labelPairs key value = [(key, value)]
 
+type Label2 = (String, String)
+
 instance Label (String, String)  where
     labelPairs (k1, k2) (v1, v2) = [(k1, v1), (k2, v2)]
 
+type Label3 = (String, String, String)
+
 instance Label (String, String, String)  where
     labelPairs (k1, k2, k3) (v1, v2, v3) = [(k1, v1), (k2, v2), (k3, v3)]
+
+type Label4 = (String, String, String, String)
 
 instance Label (String, String, String, String)  where
     labelPairs (k1, k2, k3, k4) (v1, v2, v3, v4) =
             [(k1, v1), (k2, v2), (k3, v3), (k4, v4)]
 
+type Label5 = (String, String, String, String, String)
+
 instance Label (String, String, String, String, String)  where
     labelPairs (k1, k2, k3, k4, k5) (v1, v2, v3, v4, v5) =
             [(k1, v1), (k2, v2), (k3, v3), (k4, v4), (k5, v5)]
+
+type Label6 = (String, String, String, String, String, String)
 
 instance Label (String, String, String, String, String, String)  where
     labelPairs (k1, k2, k3, k4, k5, k6) (v1, v2, v3, v4, v5, v6) =
             [(k1, v1), (k2, v2), (k3, v3), (k4, v4), (k5, v5), (k6, v6)]
 
+type Label7 = (String, String, String, String, String, String, String)
+
 instance Label (String, String, String, String, String, String, String)  where
     labelPairs (k1, k2, k3, k4, k5, k6, k7) (v1, v2, v3, v4, v5, v6, v7) =
             [(k1, v1), (k2, v2), (k3, v3), (k4, v4), (k5, v5), (k6, v6),
              (k7, v7)]
+
+type Label8 = (String, String, String, String, String, String, String, String)
 
 instance Label (String, String, String, String, String, String, String,
                 String)  where
@@ -44,6 +72,9 @@ instance Label (String, String, String, String, String, String, String,
                (v1, v2, v3, v4, v5, v6, v7, v8) =
             [(k1, v1), (k2, v2), (k3, v3), (k4, v4), (k5, v5), (k6, v6),
              (k7, v7), (k8, v8)]
+
+type Label9 = (String, String, String, String, String, String, String, String,
+               String)
 
 instance Label (String, String, String, String, String, String, String,
                 String, String)  where
