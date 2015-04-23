ba1a547bf48b7ea853112b0dd8a6f4a3cccc5f2c
Will Coster <willcoster@gmail.com>
2015-04-22 22:30:40 -0700

    Use atomic primitives for counters.
    This leads to a 10x speed improvement.

--- src/Prometheus/Metric/Counter.hs
+++ src/Prometheus/Metric/Counter.hs
@@ -7,34 +7,34 @@ module Prometheus.Metric.Counter (
 
 import Prometheus.Info
 import Prometheus.Metric
-import Prometheus.Metric.TVar
 import Prometheus.MonadMonitor
 
-import Data.Int (Int64)
-import qualified Control.Concurrent.STM as STM
+import Data.Atomics.Counter as AtomicCounter
+import qualified Data.ByteString.UTF8 as BS
 
 
-newtype Counter = MkCounter (STM.TVar Int64)
+newtype Counter = MkCounter AtomicCounter.AtomicCounter
 
 -- | Creates a new counter metric with a given name and help string.
 counter :: Info -> IO (Metric Counter)
 counter info = do
-    valueTVar <- STM.newTVarIO 0
+    atomicCounter <- AtomicCounter.newCounter 0
     return Metric {
-            handle = MkCounter valueTVar
-        ,   collect = collectTVar info CounterType valueTVar
+            handle = MkCounter atomicCounter
+        ,   collect = collectCounter info atomicCounter
         }
-
-withCounter :: MonadMonitor m => Metric Counter -> (Int64 -> Int64) -> m ()
-withCounter (Metric {handle = MkCounter valueTVar}) f =
-    doIO $ STM.atomically $ STM.modifyTVar' valueTVar f
 
 -- | Increments the value of a counter metric by 1.
 incCounter :: MonadMonitor m => Metric Counter -> m ()
-incCounter counter = withCounter counter inc
-    where inc i = i + 1
+incCounter (Metric {handle = MkCounter c}) =
+    doIO $ AtomicCounter.incrCounter_ 1 c
 
 -- | Retrieves the current value of a counter metric.
-getCounter :: Metric Counter -> IO Int64
-getCounter (Metric {handle = MkCounter valueTVar}) =
-    STM.atomically $ STM.readTVar valueTVar
+getCounter :: Metric Counter -> IO Int
+getCounter (Metric {handle = MkCounter c}) = AtomicCounter.readCounter c
+
+collectCounter :: Info -> AtomicCounter.AtomicCounter -> IO [SampleGroup]
+collectCounter info c = do
+    value <- AtomicCounter.readCounter c
+    let sample = Sample (metricName info) [] (BS.fromString $ show value)
+    return [SampleGroup info CounterType [sample]]
