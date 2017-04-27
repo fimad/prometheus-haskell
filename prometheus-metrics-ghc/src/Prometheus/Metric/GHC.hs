1c41b3da2cda44fc5fccd6d56c7a172d60ec60ab
Jonathan Lange <jml@mumak.net>
2017-04-27 10:35:16 +0100

    Name GHC metrics according to Prometheus best practices
    This is mostly adding `_total` to the Counters and moving the units toward the
    end of the metric name.
    
    See https://prometheus.io/docs/practices/naming/.

--- prometheus-metrics-ghc/src/Prometheus/Metric/GHC.hs
+++ prometheus-metrics-ghc/src/Prometheus/Metric/GHC.hs
@@ -39,7 +39,7 @@ ghcCollectors = [
             GaugeType
             getNumCapabilities
     ,   statsCollector
-            "ghc_bytes_allocated"
+            "ghc_allocated_bytes_total"
             "Total number of bytes allocated."
             CounterType
             bytesAllocated
@@ -49,78 +49,78 @@ ghcCollectors = [
             CounterType
             numGcs
     ,   statsCollector
-            "ghc_max_bytes_used"
+            "ghc_max_used_bytes"
             "The maximum number of live bytes seen so far."
             GaugeType
             maxBytesUsed
     ,   statsCollector
-            "ghc_cumulative_bytes_used"
+            "ghc_cumulative_used_bytes_total"
             "The cumulative total bytes used."
             CounterType
             cumulativeBytesUsed
     ,   statsCollector
-            "ghc_bytes_copied"
+            "ghc_copied_bytes_total"
             "The number of bytes copied during garbage collection."
             CounterType
             bytesCopied
     ,   statsCollector
-            "ghc_current_bytes_used"
+            "ghc_current_used_bytes"
             "The number of current live bytes."
             GaugeType
             currentBytesUsed
     ,   statsCollector
-            "ghc_current_bytes_slop"
+            "ghc_current_slop_bytes"
             "The current number of bytes lost to slop."
             GaugeType
             currentBytesSlop
     ,   statsCollector
-            "ghc_max_bytes_slop"
+            "ghc_max_slop_bytes"
             "The maximum number of bytes lost to slop so far."
             GaugeType
             maxBytesSlop
     ,   statsCollector
-            "ghc_peak_megabytes_allocated"
+            "ghc_peak_allocated_megabytes" -- XXX: export as bytes?
             "The maximum number of megabytes allocated."
             GaugeType
             peakMegabytesAllocated
     ,   statsCollector
-            "ghc_mutator_cpu_seconds"
+            "ghc_mutator_cpu_seconds_total"
             "The CPU time spent running mutator threads."
             CounterType
             mutatorCpuSeconds
     ,   statsCollector
-            "ghc_mutator_wall_seconds"
+            "ghc_mutator_wall_seconds_total"
             "The wall clock time spent running mutator threads."
             CounterType
             mutatorCpuSeconds
     ,   statsCollector
-            "ghc_gc_cpu_seconds"
+            "ghc_gc_cpu_seconds_total"
             "The CPU time spent running GC."
             CounterType
             gcCpuSeconds
     ,   statsCollector
-            "ghc_gc_wall_seconds"
+            "ghc_gc_wall_seconds_total"
             "The wall clock time spent running GC."
             CounterType
             gcWallSeconds
     ,   statsCollector
-            "ghc_cpu_seconds"
+            "ghc_cpu_seconds_total"
             "Total CPU time elapsed since program start."
             CounterType
             cpuSeconds
     ,   statsCollector
-            "ghc_wall_seconds"
+            "ghc_wall_seconds_total"
             "Total wall clock time elapsed since start."
             CounterType
             wallSeconds
     ,   statsCollector
-            "ghc_parallel_total_bytes_copied"
+            "ghc_parallel_copied_bytes_total"
             ("Number of bytes copied during GC, minus space held by mutable "
                 ++ "lists held by the capabilities.")
             CounterType
             parTotBytesCopied
     ,   statsCollector
-            "ghc_parallel_max_bytes_copied"
+            "ghc_parallel_max_copied_bytes_total"
             ("Sum of number of bytes copied each GC by the most active GC "
                 ++ "thread each GC.")
             CounterType
