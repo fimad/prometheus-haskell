02a0ed2ef0b54e7c4d8956f519cd19d805414385
Yghor Kerscher <kerscher@outlook.com>
2018-01-08 15:00:36 +0100

    Allow user-defined labelling functions

--- wai-middleware-prometheus/src/Network/Wai/Middleware/Prometheus.hs
+++ wai-middleware-prometheus/src/Network/Wai/Middleware/Prometheus.hs
@@ -2,16 +2,17 @@
 -- metrics and for instrumenting WAI applications.
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RecordWildCards #-}
-module Network.Wai.Middleware.Prometheus (
-    prometheus
-,   PrometheusSettings (..)
-,   Default.def
-,   instrumentApp
-,   instrumentIO
-,   metricsApp
-) where
 
-import qualified Data.ByteString.Builder as BS
+module Network.Wai.Middleware.Prometheus
+  ( prometheus
+  , PrometheusSettings(..)
+  , Default.def
+  , instrumentWithLabels
+  , instrumentApp
+  , instrumentIO
+  , metricsApp
+  ) where
+
 import qualified Data.Default as Default
 import Data.Maybe (fromMaybe)
 import Data.Ratio ((%))
@@ -54,22 +55,35 @@ requestLatency = Prom.unsafeRegister $ Prom.vector ("handler", "method", "status
     where info = Prom.Info "http_request_duration_seconds"
                            "The HTTP request latencies in seconds."
 
+-- | Instrument a WAI app with a user-defined labelling function.
+--
+-- If you use this function you will likely want to override the default value
+-- of 'prometheusInstrumentApp' to be false so that your app does not get double
+-- instrumented.
+instrumentWithLabels ::
+     (Wai.Request -> Text) -- ^ The labelling function used to identify this app
+  -> Wai.Application -- ^ The app to instrument
+  -> Wai.Application -- ^ The instrumented app
+instrumentWithLabels f app req respond = do
+  start <- getTime Monotonic
+  app req $ \res -> do
+    end <- getTime Monotonic
+    let method = Just $ decodeUtf8 (Wai.requestMethod req)
+    let status = Just $ T.pack (show (HTTP.statusCode (Wai.responseStatus res)))
+    observeSeconds (f req) method status start end
+    respond res
+
 -- | Instrument a WAI app with the default WAI metrics.
 --
 -- If you use this function you will likely want to override the default value
 -- of 'prometheusInstrumentApp' to be false so that your app does not get double
 -- instrumented.
-instrumentApp :: Text             -- ^ The label used to identify this app
-              -> Wai.Application  -- ^ The app to instrument
-              -> Wai.Application  -- ^ The instrumented app
-instrumentApp handler app req respond = do
-    start <- getTime Monotonic
-    app req $ \res -> do
-        end <- getTime Monotonic
-        let method = Just $ decodeUtf8 (Wai.requestMethod req)
-        let status = Just $ T.pack (show (HTTP.statusCode (Wai.responseStatus res)))
-        observeSeconds handler method status start end
-        respond res
+instrumentApp ::
+     Text -- ^ The label used to identify this app
+  -> Wai.Application -- ^ The app to instrument
+  -> Wai.Application -- ^ The instrumented app
+instrumentApp handler app req respond =
+  instrumentWithLabels (const handler) app req respond
 
 -- | Instrument an IO action with timing metrics. This function can be used if
 -- you would like to get more fine grained metrics, for instance this can be
