78ef91f074f438add7c0dfcfca61e8054b06c7f0
Kostiantyn Rybnikov <k-bx@k-bx.com>
2018-03-27 17:23:31 +0300

    Add GHC 8.4 support

--- prometheus-metrics-ghc/src/Prometheus/Metric/GHC.hs
+++ prometheus-metrics-ghc/src/Prometheus/Metric/GHC.hs
@@ -1,4 +1,5 @@
 {-# language OverloadedStrings #-}
+{-# language CPP #-}
 
 -- | This module defines a metrics that exposes statistics from the GHC runtime
 -- system ("GHC.Conc", "GHC.Stats").
@@ -16,7 +17,9 @@ module Prometheus.Metric.GHC (
 import Control.Applicative ((<$>))
 import qualified Data.ByteString.UTF8 as BS
 import Data.Text (Text)
+#if __GLASGOW_HASKELL__ < 804
 import GHC.Conc (numSparks, getNumCapabilities)
+#endif
 import GHC.Stats
 import Prometheus
 
@@ -26,6 +29,7 @@ data GHCMetrics = GHCMetrics
 ghcMetrics :: Metric GHCMetrics
 ghcMetrics = Metric (return (GHCMetrics, concat <$> sequence ghcCollectors))
 
+#if __GLASGOW_HASKELL__ < 804
 ghcCollectors :: [IO [SampleGroup]]
 ghcCollectors = [
         showCollector
@@ -124,7 +128,45 @@ ghcCollectors = [
             CounterType
             parMaxBytesCopied
     ]
+#else
+ghcCollectors :: [IO [SampleGroup]]
+ghcCollectors = [
+      statsCollector "gcs" "Total number of GCs" CounterType gcs
+    , statsCollector "major_gcs" "Total number of major (oldest generation) GCs" CounterType major_gcs
+    , statsCollector "allocated_bytes" "Total bytes allocated" CounterType allocated_bytes
+    , statsCollector "max_live_bytes" "Maximum live data (including large objects + compact regions)" CounterType max_live_bytes
+    , statsCollector "max_large_objects_bytes" "Maximum live data in large objects" CounterType max_large_objects_bytes
+    , statsCollector "max_compact_bytes" "Maximum live data in compact regions" CounterType max_compact_bytes
+    , statsCollector "max_slop_bytes" "Maximum slop" CounterType max_slop_bytes
+    , statsCollector "max_mem_in_use_bytes" "Maximum memory in use by the RTS" CounterType max_mem_in_use_bytes
+    , statsCollector "cumulative_live_bytes" "Sum of live bytes across all major GCs. Divided by major_gcs gives the average live data over the lifetime of the program." CounterType cumulative_live_bytes
+    , statsCollector "copied_bytes" "Sum of copied_bytes across all GCs" CounterType copied_bytes
+    , statsCollector "par_copied_bytes" "Sum of copied_bytes across all parallel GCs" CounterType par_copied_bytes
+    , statsCollector "cumulative_par_max_copied_bytes" "Sum of par_max_copied_bytes across all parallel GCs" CounterType cumulative_par_max_copied_bytes
+    , statsCollector "mutator_cpu_ns" "Total CPU time used by the mutator" CounterType mutator_cpu_ns
+    , statsCollector "mutator_elapsed_ns" "Total elapsed time used by the mutator" CounterType mutator_elapsed_ns
+    , statsCollector "gc_cpu_ns" "Total CPU time used by the GC" CounterType gc_cpu_ns
+    , statsCollector "gc_elapsed_ns" "Total elapsed time used by the GC" CounterType gc_elapsed_ns
+    , statsCollector "cpu_ns" "Total CPU time (at the previous GC)" CounterType cpu_ns
+    , statsCollector "elapsed_ns" "Total elapsed time (at the previous GC)" CounterType elapsed_ns
 
+    , statsCollector "gcdetails_gen" "The generation number of this GC" CounterType (gcdetails_gen . gc)
+    , statsCollector "gcdetails_threads" "Number of threads used in this GC" CounterType (gcdetails_threads . gc)
+    , statsCollector "gcdetails_allocated_bytes" "Number of bytes allocated since the previous GC" CounterType (gcdetails_allocated_bytes . gc)
+    , statsCollector "gcdetails_live_bytes" "Total amount of live data in the heap (incliudes large + compact data)" CounterType (gcdetails_live_bytes . gc)
+    , statsCollector "gcdetails_large_objects_bytes" "Total amount of live data in large objects" CounterType (gcdetails_large_objects_bytes . gc)
+    , statsCollector "gcdetails_compact_bytes" "Total amount of live data in compact regions" CounterType (gcdetails_compact_bytes . gc)
+    , statsCollector "gcdetails_slop_bytes" "Total amount of slop (wasted memory)" CounterType (gcdetails_slop_bytes . gc)
+    , statsCollector "gcdetails_mem_in_use_bytes" "Total amount of memory in use by the RTS" CounterType (gcdetails_mem_in_use_bytes . gc)
+    , statsCollector "gcdetails_copied_bytes" "Total amount of data copied during this GC" CounterType (gcdetails_copied_bytes . gc)
+    , statsCollector "gcdetails_par_max_copied_bytes" "In parallel GC, the max amount of data copied by any one thread" CounterType (gcdetails_par_max_copied_bytes . gc)
+    , statsCollector "gcdetails_sync_elapsed_ns" "The time elapsed during synchronisation before GC" CounterType (gcdetails_sync_elapsed_ns . gc)
+    , statsCollector "gcdetails_cpu_ns" "The CPU time used during GC itself" CounterType (gcdetails_cpu_ns . gc)
+    , statsCollector "gcdetails_elapsed_ns" "The time elapsed during GC itself" CounterType (gcdetails_elapsed_ns . gc)
+  ]
+#endif
+
+#if __GLASGOW_HASKELL__ < 804
 statsCollector :: Show a
                => Text -> Text -> SampleType -> (GCStats -> a) -> IO [SampleGroup]
 statsCollector name help sampleType stat = do
@@ -132,6 +174,15 @@ statsCollector name help sampleType stat = do
     if statsEnabled
         then showCollector name help sampleType (stat <$> getGCStats)
         else return []
+#else
+statsCollector :: Show a
+               => Text -> Text -> SampleType -> (RTSStats -> a) -> IO [SampleGroup]
+statsCollector name help sampleType stat = do
+    statsEnabled <- getRTSStatsEnabled
+    if statsEnabled
+        then showCollector name help sampleType (stat <$> getRTSStats)
+        else return []
+#endif
 
 showCollector :: Show a => Text -> Text -> SampleType -> IO a -> IO [SampleGroup]
 showCollector name help sampleType ioInt = do
