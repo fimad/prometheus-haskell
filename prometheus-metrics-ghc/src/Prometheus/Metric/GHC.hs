1aff5f48bc2724606d696c9887103d7665fdb714
Maciej Bielecki <zyla@prati.pl>
2018-05-18 22:42:26 +0200

    Fix metric names, units and types

--- prometheus-metrics-ghc/src/Prometheus/Metric/GHC.hs
+++ prometheus-metrics-ghc/src/Prometheus/Metric/GHC.hs
@@ -1,5 +1,6 @@
 {-# language OverloadedStrings #-}
 {-# language CPP #-}
+{-# language NumDecimals #-}
 
 -- | This module defines a metrics that exposes statistics from the GHC runtime
 -- system ("GHC.Conc", "GHC.Stats").
@@ -17,10 +18,14 @@ module Prometheus.Metric.GHC (
 import Control.Applicative ((<$>))
 import qualified Data.ByteString.UTF8 as BS
 import Data.Text (Text)
+import Data.Fixed (Fixed, E9)
 #if __GLASGOW_HASKELL__ < 804
 import GHC.Conc (numSparks, getNumCapabilities)
+import GHC.Stats (GCStats(..), getGCStatsEnabled, getGCStats)
+#else
+import GHC.Stats (RTSStats(..), GCDetails(..), getRTSStatsEnabled, getRTSStats)
 #endif
-import GHC.Stats
+import qualified GHC.Stats as Stats
 import Prometheus
 
 
@@ -128,28 +133,30 @@ ghcCollectors = [
             CounterType
             parMaxBytesCopied
     ]
+
 #else
+
 ghcCollectors :: [IO [SampleGroup]]
 ghcCollectors = [
       statsCollector
-            "ghc_gcs"
+            "ghc_gcs_total"
             "Total number of GCs"
             CounterType
             gcs
     , statsCollector
-            "ghc_major_gcs"
+            "ghc_major_gcs_total"
             "Total number of major (oldest generation) GCs"
             CounterType
             major_gcs
     , statsCollector
-            "ghc_allocated_bytes"
+            "ghc_allocated_bytes_total"
             "Total bytes allocated"
             CounterType
             allocated_bytes
     , statsCollector
             "ghc_max_live_bytes"
             "Maximum live data (including large objects + compact regions)"
-            CounterType
+            CounterType -- TODO: Shouldn't it be a Gauge? It only ever goes up, but it's not counting anything
             max_live_bytes
     , statsCollector
             "ghc_max_large_objects_bytes"
@@ -172,90 +179,93 @@ ghcCollectors = [
             CounterType
             max_mem_in_use_bytes
     , statsCollector
-            "ghc_cumulative_live_bytes"
+            "ghc_cumulative_live_bytes_total"
             "Sum of live bytes across all major GCs. Divided by major_gcs gives the average live data over the lifetime of the program."
             CounterType
             cumulative_live_bytes
     , statsCollector
-            "ghc_copied_bytes"
+            "ghc_copied_bytes_total"
             "Sum of copied_bytes across all GCs"
             CounterType
             copied_bytes
     , statsCollector
-            "ghc_par_copied_bytes"
+            "ghc_par_copied_bytes_total"
             "Sum of copied_bytes across all parallel GCs"
             CounterType
             par_copied_bytes
     , statsCollector
-            "ghc_cumulative_par_max_copied_bytes"
+            "ghc_cumulative_par_max_copied_bytes_total"
             "Sum of par_max_copied_bytes across all parallel GCs"
             CounterType
             cumulative_par_max_copied_bytes
     , statsCollector
-            "ghc_mutator_cpu_ns"
+            "ghc_mutator_cpu_seconds_total"
             "Total CPU time used by the mutator"
             CounterType
-            mutator_cpu_ns
+            (rtsTimeToSeconds . mutator_cpu_ns)
     , statsCollector
-            "ghc_mutator_elapsed_ns"
+            "ghc_mutator_elapsed_seconds_total"
             "Total elapsed time used by the mutator"
             CounterType
-            mutator_elapsed_ns
+            (rtsTimeToSeconds . mutator_elapsed_ns)
     , statsCollector
-            "ghc_gc_cpu_ns"
+            "ghc_gc_cpu_seconds_total"
             "Total CPU time used by the GC"
             CounterType
-            gc_cpu_ns
+            (rtsTimeToSeconds . gc_cpu_ns)
     , statsCollector
-            "ghc_gc_elapsed_ns"
+            "ghc_gc_elapsed_seconds_total"
             "Total elapsed time used by the GC"
             CounterType
-            gc_elapsed_ns
+            (rtsTimeToSeconds . gc_elapsed_ns)
     , statsCollector
-            "ghc_cpu_ns"
+            "ghc_cpu_seconds_total"
             "Total CPU time (at the previous GC)"
             CounterType
-            cpu_ns
+            (rtsTimeToSeconds . cpu_ns)
     , statsCollector
-            "ghc_elapsed_ns"
+            "ghc_elapsed_seconds_total"
             "Total elapsed time (at the previous GC)"
             CounterType
-            elapsed_ns
+            (rtsTimeToSeconds . elapsed_ns)
 
     , statsCollector
             "ghc_gcdetails_gen"
             "The generation number of this GC"
-            CounterType
+            HistogramType -- TODO: is this correct?
+                          -- Gauge makes little sense here.
+                          -- With Histogram we'll be able to see which
+                          -- generations are collected most often.
             (gcdetails_gen . gc)
     , statsCollector
             "ghc_gcdetails_threads"
             "Number of threads used in this GC"
-            CounterType
+            GaugeType
             (gcdetails_threads . gc)
     , statsCollector
             "ghc_gcdetails_allocated_bytes"
             "Number of bytes allocated since the previous GC"
-            CounterType
+            GaugeType -- TODO: this doesn't seem very meaningful.
             (gcdetails_allocated_bytes . gc)
     , statsCollector
             "ghc_gcdetails_live_bytes"
-            "Total amount of live data in the heap (incliudes large + compact data)"
-            CounterType
+            "Total amount of live data in the heap (including large + compact data)"
+            GaugeType
             (gcdetails_live_bytes . gc)
     , statsCollector
             "ghc_gcdetails_large_objects_bytes"
             "Total amount of live data in large objects"
-            CounterType
+            GaugeType
             (gcdetails_large_objects_bytes . gc)
     , statsCollector
             "ghc_gcdetails_compact_bytes"
             "Total amount of live data in compact regions"
-            CounterType
+            GaugeType
             (gcdetails_compact_bytes . gc)
     , statsCollector
             "ghc_gcdetails_slop_bytes"
             "Total amount of slop (wasted memory)"
-            CounterType
+            GaugeType
             (gcdetails_slop_bytes . gc)
     , statsCollector
             "ghc_gcdetails_mem_in_use_bytes"
@@ -265,29 +275,33 @@ ghcCollectors = [
     , statsCollector
             "ghc_gcdetails_copied_bytes"
             "Total amount of data copied during this GC"
-            CounterType
+            GaugeType -- TODO: this will also vary wildly between GCs of different generations.
             (gcdetails_copied_bytes . gc)
     , statsCollector
             "ghc_gcdetails_par_max_copied_bytes"
             "In parallel GC, the max amount of data copied by any one thread"
-            CounterType
+            GaugeType
             (gcdetails_par_max_copied_bytes . gc)
     , statsCollector
-            "ghc_gcdetails_sync_elapsed_ns"
+            "ghc_gcdetails_sync_elapsed_seconds"
             "The time elapsed during synchronisation before GC"
-            CounterType
-            (gcdetails_sync_elapsed_ns . gc)
+            GaugeType
+            (rtsTimeToSeconds . gcdetails_sync_elapsed_ns . gc)
     , statsCollector
-            "ghc_gcdetails_cpu_ns"
+            "ghc_gcdetails_cpu_seconds"
             "The CPU time used during GC itself"
-            CounterType
-            (gcdetails_cpu_ns . gc)
+            GaugeType
+            (rtsTimeToSeconds . gcdetails_cpu_ns . gc)
     , statsCollector
-            "ghc_gcdetails_elapsed_ns"
+            "ghc_gcdetails_elapsed_seconds"
             "The time elapsed during GC itself"
-            CounterType
-            (gcdetails_elapsed_ns . gc)
+            GaugeType
+            (rtsTimeToSeconds . gcdetails_elapsed_ns . gc)
   ]
+
+-- | Convert from 'RtsTime' (nanoseconds) to seconds with nanosecond precision.
+rtsTimeToSeconds :: Stats.RtsTime -> Fixed E9
+rtsTimeToSeconds = (/ 1e9) . fromIntegral
 #endif
 
 #if __GLASGOW_HASKELL__ < 804
