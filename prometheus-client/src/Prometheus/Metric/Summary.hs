e84c8ef6a7c027bf428107c9250c9a05e726b29b
Ian Duncan <ian@mercury.com>
2021-08-24 16:32:19 -0400

    Update Summary to calculate appropriate precision requirements

--- prometheus-client/src/Prometheus/Metric/Summary.hs
+++ prometheus-client/src/Prometheus/Metric/Summary.hs
@@ -26,11 +26,14 @@ import Control.Monad.Primitive
 import qualified Data.ByteString.UTF8 as BS
 import qualified Data.Text as T
 import DataSketches.Quantiles.RelativeErrorQuantile
-import DataSketches.Quantiles.RelativeErrorQuantile.Types
 import qualified DataSketches.Quantiles.RelativeErrorQuantile as ReqSketch
+import Data.Maybe (mapMaybe)
+import Prelude hiding (maximum)
+import qualified Prelude
+import Data.Word
 
-data Summary = MkSummary 
-  { reqSketch :: MVar (ReqSketch 6 (PrimState IO))
+data Summary = MkSummary
+  { reqSketch :: MVar (ReqSketch (PrimState IO))
   , quantiles :: [Quantile]
   }
 
@@ -40,32 +43,47 @@ instance NFData Summary where
 
 type Quantile = (Rational, Rational)
 
+determineK :: Quantile -> Maybe Word32
+determineK (rank_, acceptableError) = go 4
+    where
+        go k =
+            let rse = relativeStandardError (fromIntegral k) (fromRational rank_) HighRanksAreAccurate 50000
+            in if abs (rse - fromRational rank_) <= fromRational acceptableError
+                then Just k
+                else if k < 1024
+                    then go (k + 2)
+                    else Nothing
+
+
 -- | Creates a new summary metric with a given name, help string, and a list of
 -- quantiles. A reasonable set set of quantiles is provided by
 -- 'defaultQuantiles'.
 summary :: Info -> [Quantile] -> Metric Summary
 summary info quantiles_ = Metric $ do
-    -- valueTVar <- STM.newTVarIO (emptyEstimator quantiles)
-    rs <- mkReqSketch @6 HighRanksAreAccurate
-    mv <- newMVar (rs {criterion = (:<=)})
+    rs <- mkReqSketch kInt HighRanksAreAccurate
+    mv <- newMVar $ rs {criterion = (:<=)}
     let summary_ = MkSummary mv quantiles_
     return (summary_, collectSummary info summary_)
+    where
+        kInt = fromIntegral $ case mapMaybe determineK quantiles_ of
+          [] -> error "Unable to create a Summary meeting the provided quantile precision requirements"
+          xs -> Prelude.maximum xs
 
 instance Observer Summary where
     -- | Adds a new observation to a summary metric.
-    observe s v = doIO $ withMVar (reqSketch s) (`update` v)
+    observe s v = doIO $ withMVar (reqSketch s) (`ReqSketch.insert` v)
 
 -- | Retrieves a list of tuples containing a quantile and its associated value.
 getSummary :: MonadIO m => Summary -> m [(Rational, Double)]
 getSummary (MkSummary sketchVar quantiles_) = liftIO $ withMVar sketchVar $ \sketch -> do
-  forM quantiles_ $ \qv -> 
+  forM quantiles_ $ \qv ->
     (,) <$> pure (fst qv) <*> ReqSketch.quantile sketch (fromRational $ fst qv)
 
 collectSummary :: Info -> Summary -> IO [SampleGroup]
 collectSummary info (MkSummary sketchVar quantiles_) = withMVar sketchVar $ \sketch -> do
-    itemSum <- getSum sketch
-    count_ <- getN sketch
-    estimatedQuantileValues <- forM quantiles_ $ \qv -> 
+    itemSum <- ReqSketch.sum sketch
+    count_ <- ReqSketch.count sketch
+    estimatedQuantileValues <- forM quantiles_ $ \qv ->
       (,) <$> pure (fst qv) <*> ReqSketch.quantile sketch (toDouble $ fst qv)
     let sumSample = Sample (metricName info <> "_sum") [] (bsShow itemSum)
     let countSample = Sample (metricName info <> "_count") [] (bsShow count_)
